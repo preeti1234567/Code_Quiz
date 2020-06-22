bbf0cbc7b192bfc2772c681b93dab17395daa717
Preeti Gupta <preetigupta510@gmail.com>
2020-06-21 20:29:37 -0700

    first question

---
+++ script.js
@@ -0,0 +1,18 @@
+function beginQuiz()
+{
+    var showStyle=document.getElementById("quizContenthide")
+    showStyle.style.visibility="visible";
+    var contentStyle = document.getElementById("content")
+    contentStyle.style.visibility="hidden";
+
+   
+}
+
+
+
+
+
+
+
+
+
